f5af230ae7d967013b3984878888121085c555f7
Zoha Khan <zohakhan11004@gmail.com>
2024-11-10 23:17:51 +0500

    travel-website

---
+++ src/app/components/Landing.tsx
@@ -0,0 +1,8 @@
+export default function Landing() {
+  return (
+    <div className="">
+      <img src={'/HomeImage.png'} alt="Home" />
+      Hi, from landingpage
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,150 @@
-import Image from "next/image";
-
+import { MoveDown, Instagram, Twitter } from 'lucide-react';
+import Link from 'next/link';
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className='wrapper'>
+      <img className="home-image" src={'/HomeImage.png'} alt="Home" />
+      <header>
+        <div className="logo playfair-display">MNTN</div>
+        <nav>
+          <ul>
+            <Link href={'/'}><li>Home</li></Link>
+           <Link href={'/about'}><li>About us</li></Link> 
+            <li>Blog</li>
+          </ul>
+        </nav>
+        <div className="account">
+          <img src="cart.png" alt="" />
+          <p>Account</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </header>
+      <div className="center-text">
+        <div className="line-parent">
+          <div className="line"></div>
+          <h3> A HIKING GUID</h3>
+        </div>
+        <p className="paragraph playfair-display">Be Prepared For The Mountains And Beyond!</p>
+        <div className="scroll-down">
+          <Link href='#blog'>
+            <div>
+              scroll down
+            </div><i><MoveDown /> </i>
+          </Link>
+        </div>
+      </div>
+
+
+      <div className="social-links">
+        <h3 className='follow-us'>Follow Us</h3>
+        <i> <Instagram /></i>
+        <i><Twitter /></i>
+      </div>
+
+      <section id='blog'>
+
+
+        <div className="main-div-1">
+          <div className="num">
+            <h4>01</h4>
+            <div className="text-img">
+              <div className="text">
+                <div className="line-parent">
+                  <div className="line"></div>
+                  <h3>GET STARTED</h3>
+                </div>
+                <h2 className='playfair-display'>What level of  hiker are you?</h2>
+                <div className='para'>
+                  <p>Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various websites like All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker? </p>
+                </div>
+              </div>
+              <div className="image">
+                <img src="01.png" alt="" width={400} />
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+
+        <div className="main-div-2">
+          <div className="num">
+            <h4>02</h4>
+            <div className="text-img">
+              <div className="text">
+                <div className="line-parent">
+                  <div className="line"></div>
+                  <h3>HIKING ESSENTIAL</h3>
+                </div>
+                <h2 className='playfair-display'>Picking the Right Hiking Gear!</h2>
+                <div className='para'>
+                  <p>Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various websites like All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker? </p>
+                </div>
+              </div>
+              <div className="image">
+                <img src="02.png" alt="" width={400} />
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="main-div-3">
+          <div className="num">
+            <h4>03</h4>
+            <div className="text-img">
+              <div className="text">
+                <div className="line-parent">
+                  <div className="line"></div>
+                  <h3>HIKING ESSENTIAL</h3>
+                </div>
+                <h2 className='playfair-display'>Understanding your Map & Timing</h2>
+                <div className='para'>
+                  <p>Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various websites like All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker? </p>
+                </div>
+              </div>
+              <div className="image">
+                <img src="03.png" alt="" width={400} />
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+        <footer>
+          <div className='footer-log0-div'>
+            <h3 className='logo playfair-display'>MNTN</h3>
+            <h2>Get out there & discover your next slop, mountain & destination! </h2>
+          </div>
+          <div className='footer-log0-div'>
+            <h1>More on The Blog</h1>
+            <ul>
+              <li>About MNTN</li>
+              <li>Contributors & Writers</li>
+              <li>Write for Us</li>
+              <li>Contact Us</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          <div className='footer-log0-div'>
+            <h1>More on MNTN</h1>
+            <ul>
+              <li>The Team</li>
+              <li>Jobs</li>
+              <li>Press</li>
+            </ul>
+          </div>
+        </footer>
+
+      </section>
+
+    </main>
   );
 }
